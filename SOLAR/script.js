2d70d4594059e56be4070ea6b39e0f8d946f771b
Vivekrawat14 <156698854+Vivek-Rawat14@users.noreply.github.com>
2024-02-18 22:56:50 +0530

    Add files via upload

---
+++ SOLAR/script.js
@@ -0,0 +1,91 @@
+let p1 = document.getElementsByClassName("planet_desc");
+
+function s1(){
+  
+  for (i = 0; i < p1.length; i++) {
+    p1[i].style.display = "none";
+  }
+  p1[0].style.display = "block";
+}
+
+function s2() {
+  for (i = 0; i < p1.length; i++) {
+    p1[i].style.display = "none";
+  }
+  p1[1].style.display = "block";
+  
+}
+
+function s3() {
+  for (i = 0; i < p1.length; i++) {
+    p1[i].style.display = "none";
+  }
+  p1[2].style.display = "block";
+  
+}
+
+function s4() {
+  for (i = 0; i < p1.length; i++) {
+    p1[i].style.display = "none";
+  }
+  p1[3].style.display = "block";
+  
+}
+
+function s5() {
+  for (i = 0; i < p1.length; i++) {
+    p1[i].style.display = "none";
+  }
+  p1[4].style.display = "block";
+  
+}
+
+function s6() {
+  for (i = 0; i < p1.length; i++) {
+    p1[i].style.display = "none";
+  }
+  p1[5].style.display = "block";
+  
+}
+
+function s7() {
+  for (i = 0; i < p1.length; i++) {
+    p1[i].style.display = "none";
+  }
+  p1[6].style.display = "block";
+  
+}
+
+function s8() {
+  for (i = 0; i < p1.length; i++) {
+    p1[i].style.display = "none";
+  }
+  p1[7].style.display = "block";
+  
+}
+
+function s9() {
+  for (i = 0; i < p1.length; i++) {
+    p1[i].style.display = "none";
+  }
+  p1[8].style.display = "block";
+  
+}
+
+function s10() {
+  for (i = 0; i < p1.length; i++) {
+    p1[i].style.display = "none";
+  }
+  p1[9].style.display = "block";
+  
+}
+
+function s11() {
+  for (i = 0; i < p1.length; i++) {
+    p1[i].style.display = "none";
+  }
+  p1[10].style.display = "block";
+  
+}
+
+s1()
